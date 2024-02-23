a9e3186bbbb68ac3e24cb9f3e7b026a92d63ae5a
andrehebra <andrehebra@gmail.com>
2024-02-23 10:04:02 -0500

    houdini added

--- src/routes/attendance-dashboard/+page.svelte
+++ src/routes/attendance-dashboard/+page.svelte
@@ -1,14 +1,29 @@
-<script>
-    import { Label, Input } from 'flowbite-svelte';
+<script lang="ts">
+    import { Label, Input, Button } from 'flowbite-svelte';
+    import 'houdini';
+    
+    export let data;
 
-    let firstName;
-    let lastName;
+    $: ({ Cancellations } = data)
 
     
+    let firstName;
+    let lastName;
 </script>
   
-<div class="mb-6">
+<div class="mb-6 container">
     <Label for="large-input" class="block mb-2">Student Name</Label>
-    <Input id="large-input" size="lg" placeholder="First Name" />
-    <Input id="large-input" size="lg" placeholder="Last Name" />
+    <Input class="form" id="large-input" size="lg" placeholder="First Name" />
+    <Input class="form" id="large-input" size="lg" placeholder="Last Name" />
+    <Button id="submit">Generate Report</Button>
 </div>
+
+<style>
+    Label {
+        padding: 10px;
+    }
+    .form {
+        padding: 10px;
+        margin: 10px;
+    }
+</style>
