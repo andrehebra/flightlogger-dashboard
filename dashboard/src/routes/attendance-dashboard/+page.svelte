c50360e9979bb27dd3206341f8fcea286f535db8
andrehebra <andrehebra@gmail.com>
2024-02-23 09:27:09 -0500

    added all files

---
+++ dashboard/src/routes/attendance-dashboard/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Label, Input } from 'flowbite-svelte';
+
+    let firstName;
+    let lastName;
+
+    
+</script>
+  
+<div class="mb-6">
+    <Label for="large-input" class="block mb-2">Student Name</Label>
+    <Input id="large-input" size="lg" placeholder="First Name" />
+    <Input id="large-input" size="lg" placeholder="Last Name" />
+</div>
